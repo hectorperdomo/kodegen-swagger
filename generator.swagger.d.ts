970d41cfa537f91b12a4aa96762bea568d440101
hectorperdomo <hector@impactsoftware.us>
2021-09-27 07:05:09 -0400

    Delete generator.swagger.d.ts

--- generator.swagger.d.ts
+++
@@ -1,5 +0,0 @@
-import { GeneratorInterface } from '../kodegen-interface/generatorInterface';
-export declare class SwaggerGenerator implements GeneratorInterface {
-    preCompileTemplates(): void;
-    go(domainModel: any): string;
-}
